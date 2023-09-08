13da49ffc9858a7ddc7e61890b256f56d2111b4c
Goofables <Goofables@gmail.com>
2023-09-08 01:40:21 -0400

    Reformat keepalive v1
    Signed-off-by: Goofables <Goofables@gmail.com>

--- keepalive.py
+++ keepalive.py
@@ -17,26 +17,47 @@ creds_json = {"dsu_user": "user.name", "dsu_pass": "your plaintext password here
 vapp_id = "vapp-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
 
 # Get internet
-resp = requests.post("https://captiveportal.ialab.dsu.edu:6082/php/uid.php?vsys=1&rule=2", data=urlencode({
-    "inputStr": "", "escapeUser": "", "preauthid": "", "user": creds_json["dsu_user"],
-    "passwd": creds_json["dsu_pass"], "ok": "Login"}, quote_via=quote_plus))
+resp = requests.post(
+    "https://captiveportal.ialab.dsu.edu:6082/php/uid.php?vsys=1&rule=2",
+    data=urlencode(
+        {
+            "inputStr": "",
+            "escapeUser": "",
+            "preauthid": "",
+            "user": creds_json["dsu_user"],
+            "passwd": creds_json["dsu_pass"],
+            "ok": "Login",
+        },
+        quote_via=quote_plus,
+    ),
+)
 sleep(5)
 
 # Authenticate
-auth_str = '%s@%s:%s' % (creds_json["dsu_user"], creds_json["vcloud_org"], creds_json["dsu_pass"])
-resp = requests.post(url="https://vcloud.ialab.dsu.edu/api/sessions", headers={
-    "Accept": "application/*+xml;version=35.2;",
-    "Authorization": "Basic {}".format(base64.b64encode(auth_str.encode()).decode('utf-8'))})
+auth_str = "%s@%s:%s" % (creds_json["dsu_user"], creds_json["vcloud_org"], creds_json["dsu_pass"])
+resp = requests.post(
+    url="https://vcloud.ialab.dsu.edu/api/sessions",
+    headers={
+        "Accept": "application/*+xml;version=35.2;",
+        "Authorization": "Basic {}".format(base64.b64encode(auth_str.encode()).decode("utf-8")),
+    },
+)
 try:
-    auth = resp.headers['x-vcloud-authorization']
+    auth = resp.headers["x-vcloud-authorization"]
 except KeyError:
     print("Invalid Auth")
     exit()
 
 # Renew lease to 2 days
-resp = requests.put("https://vcloud.ialab.dsu.edu/api/vApp/{}/leaseSettingsSection/".format(vapp_id), json={
-    "deploymentLeaseInSeconds": "172800", "storageLeaseInSeconds": "0", "_type": "LeaseSettingsSectionType"}, headers={
-    "Content-Type": "application/*+json", "Accept": "application/*+json;version=35.2;", "x-vcloud-authorization": auth})
+resp = requests.put(
+    "https://vcloud.ialab.dsu.edu/api/vApp/{}/leaseSettingsSection/".format(vapp_id),
+    json={"deploymentLeaseInSeconds": "172800", "storageLeaseInSeconds": "0", "_type": "LeaseSettingsSectionType"},
+    headers={
+        "Content-Type": "application/*+json",
+        "Accept": "application/*+json;version=35.2;",
+        "x-vcloud-authorization": auth,
+    },
+)
 
 if resp.status_code != 202:
     print("Something broke :(")
