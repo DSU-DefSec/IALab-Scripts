18c7fe25c05e868cda93785a354cdaa6c0db90fb
Goofables <Goofables@gmail.com>
2023-11-01 01:30:29 -0400

    Changed keepalive from a crontabable script to manually started
    Signed-off-by: Goofables <Goofables@gmail.com>

--- keepalive.py
+++ keepalive.py
@@ -1,63 +1,88 @@
 #!/bin/python3
 import base64
+import datetime
+import getpass
+import random
+import time
 from time import sleep
 from urllib.parse import urlencode, quote_plus
 
 import requests
 
-## Crontab daily at 00:05
-## 5 0 * * * user /path/to/script.py
-##
-
-# Yup. Its that good     ## base64.b64decode("b64 encoded password").decode("UTF-8")
-creds_json = {"dsu_user": "user.name", "dsu_pass": "your plaintext password here", "vcloud_org": "Defsec"}
-
-#                                                                                          \  <<          This part                >> /
+VCLOUD_ORG = "Defsec"
 # https://vcloud.ialab.dsu.edu/tenant/DefSec/vdcs/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/vapp/vapp-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx/vcd-vapp-vms
-vapp_id = "vapp-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+VAPP_IDS = [
+    "vapp-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",  # Some example vapp
+]
 
-# Get internet
-resp = requests.post(
-    "https://captiveportal.ialab.dsu.edu:6082/php/uid.php?vsys=1&rule=2",
-    data=urlencode(
-        {
-            "inputStr": "",
-            "escapeUser": "",
-            "preauthid": "",
-            "user": creds_json["dsu_user"],
-            "passwd": creds_json["dsu_pass"],
-            "ok": "Login",
+
+DSU_USERNAME: str = input("Username:")
+DSU_PASSWORD: str = getpass.getpass("Password:")
+
+XOR_KEY: bytes = random.randbytes(len(DSU_PASSWORD))
+DSU_PASSWORD: list[int] = [ord(a) ^ XOR_KEY[i] for i, a in enumerate(DSU_PASSWORD)]
+
+FIRST_RUN: bool = True
+LAST = 0
+
+while True:
+    LAST = datetime.datetime.now()
+    pwd = "".join(chr(a ^ XOR_KEY[i]) for i, a in enumerate(DSU_PASSWORD))
+    # Get internet
+    requests.post(
+        "https://captiveportal.ialab.dsu.edu:6082/php/uid.php?vsys=1&rule=2",
+        data=urlencode(
+            {
+                "inputStr": "",
+                "escapeUser": "",
+                "preauthid": "",
+                "user": DSU_USERNAME,
+                "passwd": pwd,
+                "ok": "Login",
+            },
+            quote_via=quote_plus,
+        ),
+    )
+    sleep(5)
+
+    # Authenticate
+    resp = requests.post(
+        url="https://vcloud.ialab.dsu.edu/api/sessions",
+        headers={
+            "Accept": "application/*+xml;version=37.2;",
+            "Authorization": "Basic {}".format(
+                base64.b64encode(("%s@%s:%s" % (DSU_USERNAME, VCLOUD_ORG, pwd)).encode()).decode("utf-8")
+            ),
         },
-        quote_via=quote_plus,
-    ),
-)
-sleep(5)
+    )
+    try:
+        auth = resp.headers["x-vcloud-authorization"]
+    except KeyError:
+        print("Invalid Auth")
+        exit()
 
-# Authenticate
-auth_str = "%s@%s:%s" % (creds_json["dsu_user"], creds_json["vcloud_org"], creds_json["dsu_pass"])
-resp = requests.post(
-    url="https://vcloud.ialab.dsu.edu/api/sessions",
-    headers={
-        "Accept": "application/*+xml;version=35.2;",
-        "Authorization": "Basic {}".format(base64.b64encode(auth_str.encode()).decode("utf-8")),
-    },
-)
-try:
-    auth = resp.headers["x-vcloud-authorization"]
-except KeyError:
-    print("Invalid Auth")
-    exit()
+    # Refresh lease
+    for vapp in VAPP_IDS:
+        resp = requests.put(
+            "https://vcloud.ialab.dsu.edu/api/vApp/{}/leaseSettingsSection/".format(vapp),
+            # json={"deploymentLeaseInSeconds": "172800", "storageLeaseInSeconds": "0", "_type": "LeaseSettingsSectionType"},
+            json={"_type": "LeaseSettingsSectionType"},
+            headers={
+                "Content-Type": "application/*+json",
+                "Accept": "application/*+json;version=37.2;",
+                "x-vcloud-authorization": auth,
+            },
+        )
 
-# Renew lease to 2 days
-resp = requests.put(
-    "https://vcloud.ialab.dsu.edu/api/vApp/{}/leaseSettingsSection/".format(vapp_id),
-    json={"deploymentLeaseInSeconds": "172800", "storageLeaseInSeconds": "0", "_type": "LeaseSettingsSectionType"},
-    headers={
-        "Content-Type": "application/*+json",
-        "Accept": "application/*+json;version=35.2;",
-        "x-vcloud-authorization": auth,
-    },
-)
+        if resp.status_code != 202:
+            print(f"Could not refresh vapp: {vapp}")
 
-if resp.status_code != 202:
-    print("Something broke :(")
+    del pwd
+    del auth
+    del resp
+
+    if FIRST_RUN:
+        print("Renew successful!")
+
+    next_run = datetime.datetime.now() + datetime.timedelta(hours=23, minutes=30)
+    time.sleep((next_run - LAST).seconds)
